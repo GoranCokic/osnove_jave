31e4b80b5aea143054bd293c4f8b4a5e2860009d
GoranCokic <gorancokic@protonmail.com>
2023-09-21 18:37:46 +0200

    added homework d_19_09_2023

---
+++ src/main/java/d_19_09_2023/Cart.java
@@ -0,0 +1,41 @@
+package d_19_09_2023;
+
+import java.util.ArrayList;
+
+public class Cart {
+    private ArrayList<Packaging> packagings;
+
+    public Cart() {
+        this.packagings = new ArrayList<Packaging>();
+    }
+
+    public void addPackaging(Packaging packaging){
+        this.packagings.add(packaging);
+    }
+
+    public void deletePackaging(String barcode){
+        ArrayList<Packaging> listOfDeleted = new ArrayList<Packaging>();
+        for (Packaging packaging : this.packagings) {
+            if (packaging.getBarcode().equals(barcode)) {
+                listOfDeleted.add(packaging);
+            }
+        }
+        this.packagings.removeAll(listOfDeleted);
+    }
+
+    private double totalDiscountPrice(double discount) {
+        double sum = 0;
+        for (Packaging packaging : this.packagings) {
+            sum += packaging.calculatePrice();
+        }
+        return sum - discount;
+    }
+
+    public double totalCartPrice(SuperCard superCard) {
+       return this.totalDiscountPrice(superCard.getDiscount());
+    }
+
+    public ArrayList<Packaging> getPackagings() {
+        return packagings;
+    }
+}
